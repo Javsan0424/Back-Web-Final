4d731e7b209cf259583afcba33001c64ff648600
Javsan0424 <A01198909@tec.mx>
2025-04-28 08:44:56 -0600

    updated handler

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import accountRoutes from './src/routes/account'
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const port = 3001;
